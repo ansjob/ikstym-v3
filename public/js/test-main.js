f86f4f987afbc6be8d3766826ac93bcd224ca6c4
Andreas Sjöberg <andreas@tjugohundratalet.se>
2013-01-09 18:12:17 +0100

    dummy-commit

--- public/js/test-main.js
+++ public/js/test-main.js
@@ -33,6 +33,7 @@ define([
 		CalendarViewTest();
 		DateUtilTests();
 
+		//Boilerplate code below, do not touch!
 		var jasmineEnv = jasmine.getEnv();
 		jasmineEnv.updateInterval = 1000;
 		var htmlReporter = new jasmine.HtmlReporter();
